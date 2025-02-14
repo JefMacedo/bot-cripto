cc205d9dcd23b21a89463faa24fa643f8ec47bc4
Jeferson Macedo <jhef.salles@gmail.com>
2025-02-13 22:55:10 -0300

    Add order placement functionality and update RSI thresholds for trading logic

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const axios = require("axios");
+const crypto = require("crypto");
 
 const SYMBOL = 'BTCUSDT';
-
+const QUANTITY = "0.0001";
 const PERIOD = 14;
 
-const API_URL = "https://api.binance.com/";
+//CREDENCIAIS PARA ACESSO A API DE TESTE DA BINANCE
+const API_URL = "https://testnet.binance.vision";
+const API_KEY = "ZOW8InO3PzoPo2cSSOMQs5UB1Y4GHhQM6FWpKNtwAUFXrTbhP2NcWBDFq96mpwwA";
+const SECRET_KEY = "pIfidYCJkGBX5nhVZVRWVoYeseKKStKkzdURX0oMAEIPFzS46fKLsSItDTy9G7Ri";
+
+//CREDENCIAIS PARA ACESSO A API DE PRODUÇÃO DA BINANCE
+//const API_URL = "https://api.binance.com/";
 
 function averages(prices, period, startIndex) {
     let gains = 0, losses = 0;
@@ -43,13 +50,40 @@ function calcRSI(prices, period) {
     return 100 - (100 / (1 + rs));
 }
 
+async function newOrder(symbol, quantity, side){
+    const order = { symbol, quantity, side };
+    order.type = "MARKET";
+    order.timestamp = Date.now();
+
+    const signature = crypto
+        .createHmac("sha256", SECRET_KEY)
+        .update(new URLSearchParams(order).toString())
+        .digest("hex");
+
+    order.signature = signature;
+
+    try{
+        const {data} = await axios.post(
+            API_URL + "/api/v3/order", 
+            new URLSearchParams(order).toString(), 
+            {
+                headers: { "X-MBX-APIKEY": API_KEY }
+            }
+        )
+        console.log(data);
+    }
+    catch(err){
+        console.error(err.response.data);
+    }
+}
+
 let isOpened = false;
 
 async function start() {
     console.clear();
     console.log('Checking price...');
 
-    const {data} = await axios.get(`${API_URL}api/v3/klines?limit=100&interval=1m&symbol=${SYMBOL}`);
+    const {data} = await axios.get(`${API_URL}/api/v3/klines?limit=100&interval=1m&symbol=${SYMBOL}`);
     const candle = data[data.length -1];
     const lastPrice = parseFloat(candle[4]);
 
@@ -59,19 +93,25 @@ async function start() {
     const rsi = calcRSI(prices, PERIOD);
     console.log(`RSI: ${rsi}`);
 
-    if(rsi < 40 && isOpened === false) {
+    if(rsi < 35 && isOpened === false) {
         console.log('Sobrevendido, hora de comprar');
         isOpened = true;
+        
+        newOrder(SYMBOL, QUANTITY, 'BUY');
+        console.log('Ordem de compra enviada!');
     }
     else if(rsi > 70 && isOpened === true) {
         console.log('Sobrecomprado, hora de vender!');
         isOpened = false;
+        
+        newOrder(SYMBOL, QUANTITY, 'SELL');
+        console.log('Ordem de venda enviada!');
     }
     else{
         console.log('Aguardar!');
     }
 }
 
-setInterval(start, (1000 * 35));
+setInterval(start, (1000 * 5));
 
 start();
